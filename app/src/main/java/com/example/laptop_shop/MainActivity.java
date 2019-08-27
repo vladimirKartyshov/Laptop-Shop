56991b15d652d12be5c93760e2a848dae34fba59
vladimirKartyshov <vovan198080@inbox.ru>
2019-08-27 12:36:20 +0300

    Initional project

---
+++ app/src/main/java/com/example/laptop_shop/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.laptop_shop;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
