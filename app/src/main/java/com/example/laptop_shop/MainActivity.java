9b091198a40da9e76c9f17d707dd68f6bc44e50a
vladimirKartyshov <vovan198080@inbox.ru>
2019-09-02 13:35:28 +0300

    Initional project Git added XML - part1 XML - part2 XML - part3 onClick

--- app/src/main/java/com/example/laptop_shop/MainActivity.java
+++ app/src/main/java/com/example/laptop_shop/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.laptop_shop;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void increaseQuantity(View view) {
+        TextView quantityTextView = findViewById(R.id.quantityTextView);
+        quantityTextView.setText("1");
     }
 }
