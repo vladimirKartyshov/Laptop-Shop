aecbbc8309467d2722fc953f62cbf4ed35fe84c6
vladimirKartyshov <vovan198080@inbox.ru>
2019-09-20 18:16:40 +0300

    spinnerAdapter

--- app/src/main/java/com/example/laptop_shop/MainActivity.java
+++ app/src/main/java/com/example/laptop_shop/MainActivity.java
@@ -2,19 +2,43 @@ package com.example.laptop_shop;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     int quantity = 0;
+    Spinner spinner;
+    ArrayList spinnerArrayList;
+    ArrayAdapter spinnerAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+        spinnerArrayList = new ArrayList();
+
+        spinnerArrayList.add("LENOVO");
+        spinnerArrayList.add("ASUS");
+        spinnerArrayList.add("ASER");
+
+        //связываем данные со spiner
+        spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerArrayList);
+        //выставляем выпадающий список для spiner
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //устанавливаем в spiner spinerAdapter
+        spinner.setAdapter(spinnerAdapter);
+
     }
+
 
     public void increaseQuantity(View view) {
         quantity = quantity + 1;
