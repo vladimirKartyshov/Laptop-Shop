906db73f614dfdcabb6efca30424d36d60f73f19
vladimirKartyshov <vovan198080@inbox.ru>
2019-10-15 16:48:57 +0300

    send to email

--- app/src/main/java/com/example/laptop_shop/OrderActivity.java
+++ app/src/main/java/com/example/laptop_shop/OrderActivity.java
@@ -1,12 +1,19 @@
 package com.example.laptop_shop;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class OrderActivity extends AppCompatActivity {
+
+    String [] addresses = {"ikartyshov50@gmail.com"};
+    String subject = "Order from Laptop Shop";
+    String emailText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +30,25 @@ public class OrderActivity extends AppCompatActivity {
         double price = receivedOrderIntent.getDoubleExtra("price", 0);
         double orderPrice = receivedOrderIntent.getDoubleExtra("orderPrice", 0);
 
-        TextView orderTextView = findViewById(R.id.orderTextView);
-        orderTextView.setText("Customer name : " + userName + "\n" +
+        emailText = "Customer name : " + userName + "\n" +
                 "Goods name : " + goodsName + "\n" +
                 "Quantity : " + quantity + "\n" +
                 "Price : " + price + "\n" +
-                "Order Price : " + orderPrice);
+                "Order Price : " + orderPrice;
+
+        TextView orderTextView = findViewById(R.id.orderTextView);
+        orderTextView.setText(emailText);
+}
+
+    public void submitOrder(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, emailText);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
     }
 }
